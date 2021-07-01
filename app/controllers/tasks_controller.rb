482cfeddfe734d7c94ad46cdbf10ad9a6ed58b3a
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-01 09:21:35 +0000

    編集画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-before_action :set_task, only:[:show]
+before_action :set_task, only:[:show,:edit,:destoy,:update]
   def index
    @tasks = Task.all
   end
@@ -9,17 +9,35 @@ before_action :set_task, only:[:show]
   end
 
   def create
-  Task.create(task_params)
-  redirect_to tasks_path,notice:"タスクを作成"
+    @task = Task.create(task_params)
+    if @task.save
+       redirect_to tasks_path, notice: "ブログを作成しました！"
+    else
+       render :new
+    end
+  end
+  def show
   end
 
-  def show
+  def edit
+  end
+
+  def update
+      if @task.update(task_params)
+        redirect_to task_path, notice: "ブログを編集しました！"
+      else
+        render :edit
+      end
+  end
+
+  def destroy
   end
 
   private
   def task_params
     params.require(:task).permit(:name,:content)
   end
+
   def set_task
     @task = Task.find(params[:id])
   end
