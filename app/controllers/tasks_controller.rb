6c091592241761ef99c18be58983ff854537c7bf
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-01 07:10:46 +0000

    showコントローラーとviewの設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,17 @@
 class TasksController < ApplicationController
+before_action :set_taso, only:[:show]
   def index
    @tasks = Task.all
   end
 
   def show
   end
+
   private
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:name,:content)
+  end
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
