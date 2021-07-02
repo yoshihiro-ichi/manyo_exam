bb4437a9204dc065fae2d7f445ca9aa34b6e167a
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-02 14:12:54 +0000

    モデルのテストコードを記入

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :content,  length: { in: 1..140 }
   validates :content, presence: true
+  validates :name, presence: true
 end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+describe 'タスクモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空の場合' do
+      it 'バリデーションにひっかる' do
+        task = Task.new(name: '', content: '失敗テスト')
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクの詳細が空の場合' do
+      it 'バリデーションにひっかかる' do
+        task = Task.new(name: 'test' ,content: '')
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクのタイトルと詳細に内容が記載されている場合' do
+      it 'バリデーションが通る' do
+        task = Task.create(name: 'test',content:'test' )
+        expect(task).to be_valid
+      end
+    end
+  end
+end
