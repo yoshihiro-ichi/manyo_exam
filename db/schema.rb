8802b99cc48b18799beab07b01e43c7fdbed5693
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-06 05:53:46 +0000

    タスク登録画面にステータスのプルダウンを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_122759) do
+ActiveRecord::Schema.define(version: 2021_07_06_053350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_07_05_122759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline", default: "2021-07-01 00:00:00", null: false
+    t.string "status"
   end
 
 end
