f0f41b2b632451834cf80a19013298f9181c1211
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-08 14:40:14 +0000

    usermodel作成、userテーブル作成(name,email)、emailカラムにユニーク制約設定、rspecにusers.rb作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_162210) do
+ActiveRecord::Schema.define(version: 2021_07_08_143146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_07_07_162210) do
     t.integer "priority"
     t.index ["name"], name: "index_tasks_on_name"
     t.index ["status"], name: "index_tasks_on_status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
