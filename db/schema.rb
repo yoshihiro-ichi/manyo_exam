702c7b5355015b52b9c3d4d11b423f3b75c5ff95
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-10 18:20:40 +0000

    管理者ページの為にadminカラムをついか

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_214444) do
+ActiveRecord::Schema.define(version: 2021_07_10_180740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_08_214444) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "tasks", "users"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(username:  "管理者",
+             email: "admin@example.jp",
+             password:  "11111111",
+             password_confirmation: "11111111",
+             admin: true)
+
 1.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
