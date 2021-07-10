88e344dd6dcf6555a01619b029c74a3093364870
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-10 15:19:40 +0000

    user登録と同時にログイン設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+      #  ここに追記
       redirect_to user_path(@user.id)
+      flash[:notice] = 'ログインしました'
     else
       render :new
     end
