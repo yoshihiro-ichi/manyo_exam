8763317cc8538aa3dfa9f0895a0eab1b510ac15b
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-11 06:59:10 +0000

    admin/userの編集画面を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,12 +9,25 @@ class Admin::UsersController < ApplicationController
        @users = User.all.order("created_at ASC").includes(:tasks)
        @users = @users.page(params[:page]).per(8)
      end
+
+     def create
+       @user = User.new(user_params)
+       if @user.save
+         redirect_to admin_users_path, notice: "作成完了"
+       else
+         render :new
+       end
+     end
+
+     def edit
+      end
+
   private
   def if_not_admin
     redirect_to root_path unless current_user.admin?
   end
 
-  def set_yser
+  def set_user
     @user = User.find(params[:id])
   end
 end
--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,9 @@
 module Admin::UsersHelper
-end
+  def choose_admin_path
+     if action_name == "new"
+       admin_users_path
+     elsif action_name == "edit"
+       admin_user_path
+     end
+   end
+ end
