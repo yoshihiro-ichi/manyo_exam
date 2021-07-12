bee1327f759c7532e2f2c19e04f668636aaad0fb
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-07-12 02:27:09 +0000

    model callback　設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   before_validation { email.downcase! }
   has_secure_password
+  before_destroy :check_admin_destroy
+  before_update :check_admin_update
+    private
+    def check_admin_update
+     if changes.present?
+       if changes[:admin][0]
+         throw :abort if User.where(admin: true).count == 1 && self.admin == false
+       end
+     end
+   end
+
+   def check_admin_destroy
+     throw :abort if User.where(admin: true).count == 1 && self.admin? == true
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name:  "管理者",
-             email: "admin@example.jp",
-             password:  "11111111",
-             password_confirmation: "11111111",
+             email: "admin@a.com",
+             password:  "111111",
+             password_confirmation: "111111",
              admin: true)
